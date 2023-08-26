413d9f742116fd681403e426444f31ccf44712ea
4n0n <74496501+triangulair@users.noreply.github.com>
2023-08-26 15:14:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const { Client, WebhookClient } = require('discord.js-selfbot-v13');
+require('dotenv').config();
+
+const client = new Client();
+const webhookClient = new WebhookClient({
+  url: ""
+});
+
+client.on('ready', () => {
+  console.log(`${client.user.username} is ready!`);
+  console.log('Bot is running');
+});
+
+client.on('message', (msg) => {
+  if (msg.author.id === 'User ID') {
+    const message_text_author = msg.content;
+    console.log(message_text_author);
+    webhookClient.send({
+      content: message_text_author,
+      username: 'discord alert bot',
+      avatarURL: '',
+    });
+  }
+  
+});
+
+client.login('User token');
